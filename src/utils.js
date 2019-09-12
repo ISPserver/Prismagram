7168d6ff4ec3068d6ed876a1b31b6a5529e82efe
ISPserver <tjdals080@naver.com>
2019-09-12 19:02:54 +0900

    SendMail function with NodeMailer

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,35 @@
-import { adjectives, nouns } from "./words"
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 
+import { adjectives, nouns } from "./words"
+import nodemailer from "nodemailer";
+import sgTransport from "nodemailer-sendgrid-transport";
 export const generateSecret = () => {
     const randomNumber = Math.floor(Math.random() * adjectives.length);
     return `${adjectives[randomNumber]} ${nouns[randomNumber]}`
+}
+
+
+
+const sendMail = email => {
+    const options = {
+        auth: {
+            api_user: process.env.SENDGRID_USERNAME,
+            api_key: process.env.SENDGRID_PASSWORD
+        }
+    }
+    const client = nodemailer.createTransport(sgTransport(options));
+    return client.sendMail(email);
+};
+
+export const sendSecretMail = (adress, secret) => {
+    const email = {
+        from: "tjdals@prismagram.com",
+        to: adress,
+        subject: "🔒Login Secret for Prismagram🔒",
+        html: `Hello! Your login secret in ${secret}.<br/>Copy paste on the App/Website to log in`
+    };
+    return sendMail(email);
+
 }
